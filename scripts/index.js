95be4a276908226533a4b7ad92cf63cc24378472
Alexander Parfionov <parfion.alexander@yandex.ru>
2023-05-29 20:15:44 +0300

    Add picture popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-//    Popup редактирования профайла
+//    POPUP РЕДАКТИРОВАНИЯ ПРОФАЙЛА
 
 const profileEditButtonEl = document.querySelector('.profile__edit-button');
 const editPopupEl = document.querySelector('.popup__profile_edit_button');
@@ -37,9 +37,10 @@ function closePopup(popupEl) {
     popupEl.classList.remove('popup_opened');
 }
 
-//    Popup добавления 6 стандартных карточек
+//    ДОБАВЛЕНИЕ 6 СТАНДАРТНЫХ КАРТОЧЕК
 
-//    Массив с названиями и картинками карточек
+//  Массив с названиями и картинками карточек
+
 const initialCards = [
     {
       name: 'Архыз',
@@ -75,9 +76,10 @@ const initialCards = [
 
   initialCards.forEach(function (item) {
     createCard(item);                          //5. обращаюсь к функции, которую определяю ниже
-    const newCard = createCard(item);       //13. присваиваю обновленные карточки новой переменной
-    //console.log(newCloneCards);                  14. проверяю, добавился ли контент в верстку
-    templateCards.append(newCard);       //15. добавляю карточки на страницу
+
+    const newCard = createCard(item);          //13. присваиваю обновленные карточки новой переменной
+    //console.log(newCloneCards);              
+    templateCards.append(newCard);             //15. добавляю карточки на страницу
   });
 
   //6. Создал функию для заполнения карточки контентом
@@ -90,7 +92,7 @@ const initialCards = [
     nameCard.textContent = value.name;                                //10. контентом для заголовка в карточке будет имя из массива
     linkCard.src = value.link;                                        //11. контентом для заголовка в карточке будет ссылка из массива
 
-    //    Лайк карточки
+    //    ЛАЙК КАРТОЧКИ
 
     const likeButton = newCloneCard.querySelector('.element__reaction');        //1. нахожу все кнопки лайка
     //console.log(likeButton);
@@ -99,17 +101,39 @@ const initialCards = [
         likeButton.classList.toggle('element__reaction_like');                  //3. на клик вызывается функция, которая добавляет класс черного лайка
       });
 
-    //    Удаление карточки
+    //    УДАЛЕНИЕ КАРТОЧКИ
 
     const trashButton = newCloneCard.querySelector('.element__trash');          //1. нахожу все кнопки удаления карточки
     trashButton.addEventListener('click', function() {                          //2. вешаю на клик кноки действие
       templateCards.removeChild(newCloneCard);                                  //3. удаляю элемент(карточку) у блока(родителя) 
     });
 
+    //    ОТКРЫТИЕ POPUP С УВЕЛИЧЕННОЙ КАРТИНКОЙ
+
+    
+    const pictureButton = newCloneCard.querySelector('.element__pic');                  //1. нахожу все картинки на карточках
+    const popupImage = document.querySelector('#popupImage');                           //2. нахожу попап открытия картинки
+    const closeBigPicture = document.querySelector('.popup__close-button_picture');     //3. нахожу кнопки закрытия попапа
+
+    pictureButton.addEventListener('click', function() {                  //4. вешаю слушатель на нажатие картинки
+      openPopup(popupImage);                                              //5. вызываю функцию открытия попапа
+
+      const bigPicrure = document.querySelector('.popup__picture');       //6. нахожу класс с картинкой в попапе
+      const bigPictureName = document.querySelector('.popup__name');      //7. нахожу класс с названием в попапе
+      
+
+      bigPicrure.src = linkCard.src;                                      //8. большой картинкой для попапа будет картинка из карточки
+      bigPictureName.textContent = nameCard.textContent;                  //9. текстом для попапа будет название из карточки
+    });
+    
+    closeBigPicture.addEventListener('click', function() {                //10. вешаю слушатель на кнопку закрытия попапа
+      closePopup(popupImage);                                             //11. вызываю функцию закрытия попапа
+    });
+
     return newCloneCard;     //12. возвращаю карточку с элементами
   };
 
-  //    Откытие и закрытие Popup для добавления новых карточек
+  //    ОТКРЫТИЕ И ЗАКРЫТИЕ POPUP ДЛЯ ДОБАВЛЕНИЯ НОВЫХ КАРТОЧЕК
 
   const profileAddButtonEl = document.querySelector('.profile__add-button');
   const addPopupEl = document.querySelector('.popup__profile_add_button');
@@ -123,17 +147,16 @@ const initialCards = [
     closePopup(addPopupEl);
   });
 
+  //    ДОБАВЛЕНИЕ КАРТОЧКИ
 
-  //    Добавление карточки
-
-  const nameCard = document.querySelector('.popup__input_value_name-card');         //1. нахожу поле, в котором пользователь указывает имя
+  const nameCard = document.querySelector('.popup__input_value_name-card');             //1. нахожу поле, в котором пользователь указывает имя
   const picturelinkCard = document.querySelector('.popup__input_value_picture-card');   //2. нахожу поле, в котором пользователь указывает ссылку на картинку
-  const formCards = document.querySelector('.popup__form_cards')                    //3. нахожу форму
+  const formCards = document.querySelector('.popup__form_cards')                        //3. нахожу форму
 
-    formCards.addEventListener('submit', (event) => {                               //4. вешаю обработчик на форму
-      event.preventDefault();                                                         //5. убираю отправку формы
+  formCards.addEventListener('submit', (event) => {                                   //4. вешаю обработчик на форму
+      event.preventDefault();                                                           //5. убираю отправку формы
   
-      closePopup(addPopupEl);                                                         //6. вызываю функцию закрытия попапа
+      closePopup(addPopupEl);                                                           //6. вызываю функцию закрытия попапа
 
     const addNewCard = createCard({               //7. вызываю функцию клонирования, в которую передаю новые аргументы
       name: nameCard.value,                       //8. значением name будет введенное имя
@@ -141,3 +164,8 @@ const initialCards = [
     });
     templateCards.prepend(addNewCard);            //10. добавляю карточку в начало блока
   });
+  
+
+
+
+  
