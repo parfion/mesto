6347c2418279d42fc7c4b958e9c0a895b980280b
Alexander Parfionov <parfion.alexander@yandex.ru>
2023-06-16 20:21:56 +0300

    fixed review comments

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,4 @@
 //  переменные для попапа редактирования профиля
-
 const profileEditButtonEl = document.querySelector('.profile__edit-button');
 const editPopupEl = document.querySelector('#editButton');
 const popupCloseButtonEl = document.querySelector('.popup__close-button');
@@ -8,21 +7,35 @@ const popupNameEl = document.querySelector('.popup__input_value_name');
 const profileInfoProfessionEl = document.querySelector('.profile__info-profession');
 const popupProfessionEl = document.querySelector('.popup__input_value_profession');
 const popupFormEl = document.querySelector('.popup__form');
-
+const saveButton = document.querySelector('#saveButton');
+const formProfile = document.querySelector('#formProfile');
 //  переменные для попапа с картинкой
-
 const popupImage = document.querySelector('#popupImage');                           
 const closeBigPicture = document.querySelector('.popup__close-button_picture');     
 const bigPicrure = document.querySelector('.popup__picture');                       
 const bigPictureName = document.querySelector('.popup__name');                     
-
+//  переменные для попапа с добавлением карточек 
+const templateCards = document.querySelector('.elements');             
+const templateContent = templateCards.content;                         
+const templateCard = templateContent.querySelector('.element');  
+// переменные для попапа новых карточек
+const profileAddButtonEl = document.querySelector('.profile__add-button');
+const addPopupEl = document.querySelector('#addButton');
+const closeButtonAdd = document.querySelector('.popup__close-button_add');
+const addCardButton = document.querySelector('#addCardButton');
+// переменные для добавления новой карточки
+const nameCard = document.querySelector('.popup__input_value_name-card');             
+const picturelinkCard = document.querySelector('.popup__input_value_picture-card');   
+const formCards = document.querySelector('.popup__form_cards')                        
 
 //  ф-я-обработчик (СЛУШАТЕЛЬ) для кнопки редактирования профайла (открытие попап)
 
 profileEditButtonEl.addEventListener('click', () => {
     openPopup(editPopupEl);
-    valuesInput();                                                                            // задаю значения инпутам
-    document.querySelector('.popup__save-button').classList.remove('popup_button_inactive');  // удаляю класс неактивных стилей
+    valuesInput();                                                                           
+    hideInputError(popupFormEl, popupNameEl, validation);
+    hideInputError(popupFormEl, popupProfessionEl, validation);
+    deleteErrorClass(saveButton, validation);  
 });
 
 //  ф-я-обработчик (СЛУШАТЕЛЬ) для кнопки редактирования профайла (закрытие попап)
@@ -32,29 +45,27 @@ popupCloseButtonEl.addEventListener('click', () => closePopup(editPopupEl));
 //  ф-я, которая задает значения инпутам, при открытии попапа
 
 function valuesInput() {
-    popupNameEl.value = profileInfoNameEl.textContent;                // значением первого инпута будет имя профиля из html
-    popupProfessionEl.value = profileInfoProfessionEl.textContent;    // значением второго инпута будет название професси из html
+    popupNameEl.value = profileInfoNameEl.textContent;               
+    popupProfessionEl.value = profileInfoProfessionEl.textContent;   
 };
-
-valuesInput();    //  сразу, при загрузке страницы вызываю ф-ю
 
 //   ф-я отправки формы на сервер
 
 popupFormEl.addEventListener('submit', function(event) {
-    event.preventDefault();                                           // отменяю стандартное действие сабмита
+    event.preventDefault();                                           
 
-    profileInfoNameEl.textContent = popupNameEl.value;                // применяю значения инпута в профиле
+    profileInfoNameEl.textContent = popupNameEl.value;                
     profileInfoProfessionEl.textContent = popupProfessionEl.value;
 
-    closePopup(editPopupEl);                                          // закрываю попап, сохраняя введенные данные
+    closePopup(editPopupEl);                                          
 });
 
 //  ф-я (УНИВЕРСАЛЬНАЯ) открытия любого из попапов
 
 function openPopup(popupEl) {
-    popupEl.classList.add('popup_opened');                        // добавляю класс открытого попапа
-    document.addEventListener('keydown', closePopupByEsc);        // вешаю слушатель на весь документ (вызыва ф-ю закрытия попапа на Esc)
-    document.addEventListener('click', closePopupByOverlay);      // вешаю слушатель на весь документ (вызыва ф-ю закрытия попапа на Overlay)
+    popupEl.classList.add('popup_opened');                        
+    document.addEventListener('keydown', closePopupByEsc);        
+    document.addEventListener('click', closePopupByOverlay);      
   };
 
 //  ф-я (УНИВЕРСАЛЬНАЯ) закрытия любого из попапов
@@ -63,164 +74,112 @@ function closePopup(popupEl) {
     popupEl.classList.remove('popup_opened');
     document.removeEventListener('keydown', closePopupByEsc);         
     document.removeEventListener('click', closePopupByOverlay);
-    
-    deleteInputError();                                           // удаляю оишбки 
 }
 // --------------------------------------------------------------------------------------- //
 
 /////    ДОБАВЛЕНИЕ 6 СТАНДАРТНЫХ КАРТОЧЕК
 
-// 1. переменные для попапа с добавлением карточек 
-
-const templateCards = document.querySelector('.elements');             //1.1 нахожу блок, в который будут добавляться карточки
-const templateContent = templateCards.content;                         //1.2 Нахожу содержимое блока
-const templateCard = templateContent.querySelector('.element');        //1.3 Нахожу содержимое карточки
-
-//  2. ф-я заполнения карточки контентом (клоинирование)
+//   ф-я заполнения карточки контентом (клоинирование)
 
 function createCard(value) {
-    const newCloneCard = templateCard.cloneNode(true);                 //2.1 клонирую элементы карточки (true-полное клонирование)
-    const nameCard = newCloneCard.querySelector('.element__name');     //2.2 нахожу элемент в карточке, в который будет записан заголовок
-    const linkCard = newCloneCard.querySelector('.element__pic');      //2.3 нахожу элемент в карточке, в который будет записана картинка
+  // клонирую элементы карточки (true-полное клонирование)
+    const newCloneCard = templateCard.cloneNode(true);                 
+    const nameCard = newCloneCard.querySelector('.element__name');     
+    const linkCard = newCloneCard.querySelector('.element__pic');      
 
-    nameCard.textContent = value.name;                                 //2.4 контентом для заголовка в карточке будет имя из массива
-    linkCard.src = value.link;                                         //2.5 контентом для заголовка в карточке будет ссылка из массива
+    nameCard.textContent = value.name;                                 
+    linkCard.src = value.link;                                         
 
     //    ЛАЙК КАРТОЧКИ
+    const likeButton = newCloneCard.querySelector('.element__reaction');       
 
-    const likeButton = newCloneCard.querySelector('.element__reaction');       // нахожу все кнопки лайка
-
-    //  5. вешаю слушатель на кнопку лайка
     likeButton.addEventListener('click', function () {                          
-        likeButton.classList.toggle('element__reaction_like');      //5.1 на клик вызывается функция, которая добавляет класс черного лайка
+        likeButton.classList.toggle('element__reaction_like');      
     });
 
     //    УДАЛЕНИЕ КАРТОЧКИ
+    const trashButton = newCloneCard.querySelector('.element__trash');       
 
-    const trashButton = newCloneCard.querySelector('.element__trash');       // нахожу все кнопки удаления карточки
-
-    //  6. вешаю слушатель на кнопку удаления карточки
     trashButton.addEventListener('click', function() {                            
-      templateCards.removeChild(newCloneCard);                    //6.1 удаляю элемент(карточку) у блока(родителя) 
+      templateCards.removeChild(newCloneCard);                    
     });
 
     //    ОТКРЫТИЕ POPUP С УВЕЛИЧЕННОЙ КАРТИНКОЙ
+    const pictureButton = newCloneCard.querySelector('.element__pic');      
     
-    const pictureButton = newCloneCard.querySelector('.element__pic');      // нахожу все картинки на карточках
-    
-    //  7. вешаю слушатель на нажатие картинки
     pictureButton.addEventListener('click', function() {                    
-        openPopup(popupImage);                                              //7.1 вызываю функцию открытия попапа
+        openPopup(popupImage);                                              
 
-        bigPicrure.src = linkCard.src;                                      //7.2 большой картинкой для попапа будет картинка из карточки
-        bigPictureName.textContent = nameCard.textContent;                  //7.3 текстом для попапа будет название из карточки
+        bigPicrure.src = linkCard.src;                                      
+        bigPictureName.textContent = nameCard.textContent;                  
     });
 
-    //  3. ВОЗВРАЩАЮ УЖЕ ЗАПОЛНЕННУЮ КАРТОЧКУ
-    
+    //  ВОЗВРАЩАЮ УЖЕ ЗАПОЛНЕННУЮ КАРТОЧКУ
     return newCloneCard;                                                    
   };
 
-//4. прохожусь по каждому элементу массива
-
+// прохожусь по каждому элементу массива
 initialCards.forEach(function (item) {
-  createCard(item);                          //4.1 ВЫЗЫВАЮ Ф-Ю ЗАПОЛНЕННОЙ КАРТОЧКИ
+  createCard(item);                          
 
-  const newCard = createCard(item);          //4.2 присваиваю эту функцию новой переменной
+  const newCard = createCard(item);          
                 
-  templateCards.append(newCard);             //4.3 ДОБАВЛЯЮ КАРТОЧКИ НА СТРАНИЦУ (В HTML)
+  templateCards.append(newCard);             
 });
-  
 
 //   ОТКРЫТИЕ И ЗАКРЫТИЕ POPUP ДЛЯ ДОБАВЛЕНИЯ НОВЫХ КАРТОЧЕК
 
-const profileAddButtonEl = document.querySelector('.profile__add-button');
-const addPopupEl = document.querySelector('#addButton');
-const closeButtonAdd = document.querySelector('.popup__close-button_add');
-
-//  8. вешаю слушатель на кнопку добавления новой карточки (открытие попапа)
-
 profileAddButtonEl.addEventListener('click', () => {
   formCards.reset();
-  
   openPopup(addPopupEl);
+  hideInputError(formCards, nameCard, validation);
+  hideInputError(formCards, picturelinkCard, validation);
+  addErrorClass(addCardButton, validation);
 });
-
-//  9. вешаю слушатель на кнопку добавления новой карточки (закрытие попапа)
 
 closeButtonAdd.addEventListener('click', () => closePopup(addPopupEl));
 
 //     ДОБАВЛЕНИЕ НОВОЙ КАРТОЧКИ
 
-const nameCard = document.querySelector('.popup__input_value_name-card');             //10.1 нахожу поле, в котором пользователь указывает имя
-const picturelinkCard = document.querySelector('.popup__input_value_picture-card');   //10.2 нахожу поле, в котором пользователь указывает ссылку на картинку
-const formCards = document.querySelector('.popup__form_cards')                        //10.3 нахожу форму
-
-//  10.4 ф-я для отправки формы на сервер
-
 formCards.addEventListener('submit', (evt) => {                                     
-    evt.preventDefault();                                                             //10.5 убираю перезагрузку страницы
-  
-    closePopup(addPopupEl);                                                           //10.6 вызываю функцию закрытия попапа
+    evt.preventDefault();                                                          
+    closePopup(addPopupEl);                            
 
-    const addNewCard = createCard({                 //10.7 вызываю функцию клонирования, в которую передаю новые аргументы
-        name: nameCard.value,                       //10.8 значением name будет введенное имя
-        link: picturelinkCard.value                 //10.9 значением name будет введенная ссылка
+    const addNewCard = createCard({                
+        name: nameCard.value,                      
+        link: picturelinkCard.value                 
     });
 
-    templateCards.prepend(addNewCard);              //10.10 ДОБАВЛЯЮ НОВУЮ СОЗДАННУЮ КАРТОЧКУ В НАЧАЛО БЛОКА
+    templateCards.prepend(addNewCard);              
     evt.target.reset();
-    deleteInputError();
 });
 
 //    ЗАКРЫТИЕ ПОПАПА С КАРТИНКОЙ
 
-// 11. вешаю слушатель на кнопку закрытия попапа
-
 closeBigPicture.addEventListener('click', function() {      
-    closePopup(popupImage);                                  //11.1 вызываю функцию закрытия попапа
+    closePopup(popupImage);                             
 });
 
 //------------------------------------------------------------------------------------------------------- //
 
 //  ЗАКРЫТИЕ ПОПАПОВ НА ESC
-
 //  ф-я закрытия попапа на Esc (вызываю ее при открытии и закрытии попапа)
 
 function closePopupByEsc(evt) {
-    const openedPopup = document.querySelector('.popup_opened');        // ищу открытый попап по открытому классу
-    if (evt.key === 'Escape') {                                         // если нажата клавиша Escape
-      
-      closePopup(openedPopup);                                          // вызываю универсальную ф-ю закрытия попапа
+    const openedPopup = document.querySelector('.popup_opened');        
+    if (evt.key === 'Escape') {                                         
+      closePopup(openedPopup);                                          
     };
   };
 
   //  ЗАКРЫТИЕ ПОПАПОВ НА OVERLAY
-
   //  ф-я закрытия попапа на Overlay (вызываю ее при открытии и закрытии попапа)
 
   function closePopupByOverlay() {
     const openedPopup = document.querySelector('.popup_opened');
-    openedPopup.addEventListener("click", (evt) => {                  // вешаю слушатель клика на открытый попап
-      if (evt.currentTarget === evt.target) {                         // если эл-т, где сработал обработчик(родитель)ю равен эл-ту, где возникло событие
-        closePopup(openedPopup);                                      // тогда закрываю попап
+    openedPopup.addEventListener("click", (evt) => {                  
+      if (evt.currentTarget === evt.target) {                         
+        closePopup(openedPopup);                                      
       };
-    });
-  };
-
-  //  ф-я, которая убирает ошибки и их содержимое в элементах формы
-
-  function deleteInputError() {                                                                   
-    const inputList = Array.from(document.querySelectorAll('.popup__input'));
-    inputList.forEach((inputElement) => inputElement.classList.remove('popup_input-error'));
-
-    const spanList = Array.from(document.querySelectorAll('.form__type-error'));
-    spanList.forEach((spanElement) => spanElement.classList.remove('form__input-error_active'));
-    spanList.forEach((spanElement) => spanElement.textContent = '');
-
-    const buttonList = Array.from(document.querySelectorAll('.button'));
-    buttonList.forEach((buttonElement) => {
-      buttonElement.classList.add('popup_button_inactive');
-      buttonElement.setAttribute('disabled', 'true');
     });
   };
--- scripts/validate.js
+++ scripts/validate.js
@@ -11,83 +11,94 @@ const validation = {
     errorClass: 'form__input-error_active'
   };
 
-//  1. ф-я добавления классов с ошибкой
+//   ф-я добавления классов с ошибкой
 
 const showInputError = (formElement, inputElement, errorMessage, validation) => {              
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);    //1.1 нахожу инпут по id
-
-    inputElement.classList.add(validation.inputErrorClass);                         //1.2 добавляю класс красного подчеркивания в инпут
-    errorElement.textContent = errorMessage;                                        //1.3 текстом ошибки(спана) будет стандартный текст браузера
-    errorElement.classList.add(validation.errorClass);                              //1.4 добавляю класс стилей текста ошибки
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);    
+    inputElement.classList.add(validation.inputErrorClass);                         
+    errorElement.textContent = errorMessage;                                        
+    errorElement.classList.add(validation.errorClass);                              
 };
 
-//  2. ф-я удаления классов с ошибкой
+//   ф-я удаления классов с ошибкой
 
     const hideInputError = (formElement, inputElement, validation) => {                             
-    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);        //2.1 еще раз нахожу инпут по id
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);        
 
-    inputElement.classList.remove(validation.inputErrorClass);                          //2.2 удаляю класс красного подчеркивания
-    errorElement.classList.remove(validation.errorClass);                               //2.3 удаляю класс стилей текста ошибки
-    errorElement.textContent = '';                                                      //2.4 текстом ошибки(спана) будет пустая строка
+    inputElement.classList.remove(validation.inputErrorClass);                          
+    errorElement.classList.remove(validation.errorClass);                               
+    errorElement.textContent = '';                                                      
 };
 
-//  3. ф-я проверки инпута на валидность
+//   ф-я проверки инпута на валидность
 
 const checkInputValidity = (formElement, inputElement, validation) => {                         
-    if(!inputElement.validity.valid) {                                                              //3.1 если инпут невалиден
-        showInputError(formElement, inputElement, inputElement.validationMessage, validation);      //3.2 вызываю ф-ю добавления классов ошибки
+    if(!inputElement.validity.valid) {                                                             
+        showInputError(formElement, inputElement, inputElement.validationMessage, validation);      
     }
-    else {                                                                                          //3.3 иначе
-        hideInputError(formElement, inputElement, validation);                                      //3.4 вызываю ф-ю удаления классов ошибки
+    else {                                                                                          
+        hideInputError(formElement, inputElement, validation);                                      
     };
 }
 
-//  4. ф-я одновременной валидации всех инпутов для блокировки/разблокировки кнопки
+//   ф-я одновременной валидации всех инпутов для блокировки/разблокировки кнопки
 
 const hasInvalidInput = (inputList) => {                                            
-    return inputList.some((inputElement) => {           //4.1 методом some прохожусь по массиву инпутов
-        return !inputElement.validity.valid             //4.2 если хотя бы один инпут невалиден возвращаю true и прекащаю проверку 
+    return inputList.some((inputElement) => {           
+        return !inputElement.validity.valid             
     });
 };
 
-//  5. ф-я блокировки/разблокировки кнопки
+//  ф-я добавления ошибок
+
+const addErrorClass = (buttonElement, validation) => {
+    buttonElement.classList.add(validation.inactiveButtonClass);                
+    buttonElement.setAttribute('disabled', 'true'); 
+}
+
+//  ф-я удаления ошибок
+
+const deleteErrorClass = (buttonElement, validation) => {
+    buttonElement.classList.remove(validation.inactiveButtonClass);             
+    buttonElement.removeAttribute('disabled'); 
+}
+
+//   ф-я блокировки/разблокировки кнопки
 
 const toggleButtonState = (inputList, buttonElement, validation) => {                          
-    if (hasInvalidInput(inputList)) {                                               //5.1 если хотя бы одно из инпутов невалидно
-        buttonElement.classList.add(validation.inactiveButtonClass);                //5.2 добавляю класс стилей неактивной кнопки
-        buttonElement.setAttribute('disabled', 'true');                             //5.3 добавляю атрибут неактивной кнопки
-    } else {                                                                        //5.4 иначе
-        buttonElement.classList.remove(validation.inactiveButtonClass);             //5.5 удаляю класс стилей неактивной кнопки
-        buttonElement.removeAttribute('disabled');                                  //5.6 удаляю атрибут неактивной кнопки
+    if (hasInvalidInput(inputList)) {                                               
+        addErrorClass(buttonElement, validation);                          
+    } else {                                                                        
+        deleteErrorClass(buttonElement, validation);                             
     };
 };
 
-//  6. функция-обработчик (СЛУШАТЕЛЬ) для всех инпутов
+//   функция-обработчик (СЛУШАТЕЛЬ) для всех инпутов
 
 const setEventListeners = (formElement, validation) => {                                      
-    const inputList = Array.from(formElement.querySelectorAll(validation.inputSelector));       //6.1 нахожу все инпуты и делаю из них массив
-    const buttonElement = formElement.querySelector(validation.submitButtonSelector);           //6.2 нахожу кропку в элементе формы
+    const inputList = Array.from(formElement.querySelectorAll(validation.inputSelector));       
+    const buttonElement = formElement.querySelector(validation.submitButtonSelector);           
     
-    toggleButtonState(inputList, buttonElement, validation);                   //6.7 вызываю здесь ф-ю блок-и/разблок-и кнопки, чтобы шла проверка при загрузке страницы
+    toggleButtonState(inputList, buttonElement, validation);                   
 
-    inputList.forEach((inputElement) => {                                      //6.3 прохожусь по каждому элементу массива инпутов
-        inputElement.addEventListener('input', () => {                         //6.4 при каждом нажатии клавиши в инпуте
-            checkInputValidity(formElement, inputElement, validation)          //6.5 вызываю ф-ю проверки инпута на валидность
+    inputList.forEach((inputElement) => {                                      
+        inputElement.addEventListener('input', () => {                         
+            checkInputValidity(formElement, inputElement, validation)          
 
-            toggleButtonState(inputList, buttonElement, validation);           //6.6 вызываю здесь ф-ю блок-и-разблок-и кнопки, при каждом нажатии клавиши
+            toggleButtonState(inputList, buttonElement, validation);           
         });
     });
 }; 
 
-//  7. ф-я валидации всех форм
+//   ф-я валидации всех форм
 
 function enableValidation(validation) {                                                                                    
-    const formList = Array.from(document.querySelectorAll(validation.formSelector));         // 7.1 нахожу все формы и делаю из них массив
-    formList.forEach((formElement) => {                                                      // 7.2 прохожусь по каждому элементу массива с формами
-        setEventListeners(formElement, validation);                                          // 7.3 вызываю ф-ю обработчик во всех формах
+    const formList = Array.from(document.querySelectorAll(validation.formSelector));         
+    formList.forEach((formElement) => {                                                      
+        setEventListeners(formElement, validation);                                          
     }); 
   };
 
-  //  8. ВЫЗОВ Ф-И ВАЛИДАЦИИ
+  //   ВЫЗОВ Ф-И ВАЛИДАЦИИ
 
   enableValidation(validation);     
